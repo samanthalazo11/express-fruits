09d065a9b668bf87964097e6edce8ff053a86c8d
Samantha Lazo <lazoc.samantha@gmail.com>
2023-01-07 11:46:04 -0500

    wip: begin process of mvc organ

---
+++ models/fruits.js
@@ -0,0 +1,20 @@
+const fruits = [
+    {
+    "name": "apple",
+    "color": "red",
+    "readyToEat": false
+    },
+    {
+    "name": "banana",
+    "color": "yellow",
+    "readyToEat": true
+    },
+    {
+    "name": "pear",
+    "color": "green",
+    "readyToEat": true
+    }
+    ];
+    console.log(module)
+
+    module.exports = fruits;
